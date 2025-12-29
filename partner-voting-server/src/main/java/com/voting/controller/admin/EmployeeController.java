8e0149962e774bfc6fa7ea7ba47a7ab729ede75e
tangcdut <yunlong.tang@rklink.cn>
2025-12-29 22:30:21 +0800

    再次清理多余的文件

--- partner-voting-server/src/main/java/com/voting/controller/admin/EmployeeController.java
+++ partner-voting-server/src/main/java/com/voting/controller/admin/EmployeeController.java
@@ -77,55 +77,13 @@ public class EmployeeController {
     public Result<String> logout() {
         return Result.success();
     }
-@PostMapping
-@ApiOperation("新增员工")
-    public Result save(@RequestBody EmployeeDTO employeeDTO){
-        log.info("新增员工:{}",employeeDTO);
-        employeeService.save(employeeDTO);
-  return Result.success();
-    }
-/**
- * 员工分页查询
- */
-@GetMapping("/page")
-@ApiOperation("员工分页查询")
-public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
-    log.info("员工分页查询：{}",employeePageQueryDTO);
-   PageResult pageResult=employeeService.pageQuery(employeePageQueryDTO);
-    return Result.success(pageResult);
-}
 
-    /**
-     * 启用禁用员工账号
-     * @param status
-     * @param id
-     * @return
-     */
-    @PostMapping("/status/{status}")
-    @ApiOperation("启用禁用员工账号")
-    public Result StartOrStop(@PathVariable Integer status,Long id){
-        log.info("启用禁用员工账号：{},{}",status,id);
-        employeeService.StartOrStop(status,id);
-    return Result.success();
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工:{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
     }
-    /**
-     * 根据Id查询员工信息
-     */
-    @GetMapping("/{id}")
-    @ApiOperation("根据Id查询员工信息")
-    public Result<Employee> getById(@PathVariable Long id){
-        Employee employee=employeeService.getById(id);
-        return Result.success(employee);
-    }
-    /**
- *编辑员工信息
- *
- */
-    @PutMapping
-    @ApiOperation("编辑员工信息")
-public Result update(@RequestBody EmployeeDTO employeeDTO){
-    log.info("编辑员工信息：{}",employeeDTO);
-    employeeService.update(employeeDTO);
-    return Result.success();
-}
+
 }
